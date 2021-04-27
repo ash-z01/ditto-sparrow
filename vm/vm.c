7ff867275aa7a4c621496cf8450814c4daede2d6
rubik <ash-z01@qq.com>
2021-04-27 18:56:35 +0800

    alter DirStructure, add vm

---
+++ vm/vm.c
@@ -0,0 +1,22 @@
+#include "vm.h"
+#include <stdlib.h>
+#include <utils.h>
+
+
+// 初始化虚拟机
+void initVM(VM* vm) {
+    vm->allocatedBytes = 0;
+    vm->curLexer = NULL;
+}
+
+
+// 新建虚拟机
+VM* newVM() {
+    VM* vm = (VM*)malloc(sizeof(VM));
+    if (vm == NULL) {
+        MEM_ERROR("allocate VM failed!");
+    }
+    initVM(vm);
+    return vm;
+}
+
---
+++ vm/vm.h
@@ -0,0 +1,25 @@
+#ifndef _VM_VM_H
+#define _VM_VM_H
+
+
+#include "common.h"
+
+
+// 虚拟机结构
+struct vm
+{
+    // 累计分配的内存大小
+    uint32_t allocatedBytes;
+    // 当前的词法分析器
+    Lexer* curLexer;
+};
+
+
+// 初始化虚拟机
+void initVM(VM* vm);
+
+// 新建虚拟机
+VM* newVM(void);
+
+
+#endif
